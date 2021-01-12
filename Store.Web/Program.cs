653a180eef86d874048321fe0cf14cea80f47f7b
Hugo <Hugo.Goncalves.Lima@formandos.cinel.pt>
2021-01-12 20:55:13 +0000

    Create Products Seed

--- Store.Web/Program.cs
+++ Store.Web/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Store.Web.Data;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -14,7 +16,20 @@ namespace Store.Web
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = CreateWebHostBuilder(args).Build();
+            RunSeeding(host);
+            host.Run();
+            
+        }
+
+        private static void RunSeeding(IWebHost host)
+        {
+            var scopeFactory = host.Services.GetService<IServiceScopeFactory>();
+            using(var scope = scopeFactory.CreateScope())
+            {
+                var seeder = scope.ServiceProvider.GetService<SeedDb>();
+                seeder.SeedAsync().Wait();
+            }
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
