c7789dd8d6ae681e102f94d6a2b7cd67b7a9a72d
Hugo <Hugo.Goncalves.Lima@formandos.cinel.pt>
2021-01-14 16:01:58 +0000

    Products Controler API.

---
+++ Store.Web/Controllers/API/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿namespace Store.Web.Controllers.API
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Store.Web.Data;
+
+    [Route("api/[Controller]")]
+    public class ProductsController : Controller
+    {
+        private readonly IProductRepository productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            this.productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public IActionResult GetProduct()
+        {
+            return Ok(this.productRepository.GetAll());
+        }
+
+    }
+}
