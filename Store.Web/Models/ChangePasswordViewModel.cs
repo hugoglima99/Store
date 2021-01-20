23a1686b6b2520c5012ef7871812fc52434b1aaf
Hugo <Hugo.Goncalves.Lima@formandos.cinel.pt>
2021-01-20 17:28:17 +0000

    Web change user and password

---
+++ Store.Web/Models/ChangePasswordViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Store.Web.Models
+{
+    public class ChangePasswordViewModel
+    {
+        [Required]
+        [Display(Name = "Current password")]
+        public string OldPassword { get; set; }
+
+        [Required]
+        [Display(Name = "New password")]
+        public string NewPassword { get; set; }
+
+        [Required]
+        [Compare( "Current password")]
+        public string Confirm { get; set; }
+    }
+}
